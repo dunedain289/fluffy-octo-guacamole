6fafe938d7a2c65d951652715dc0fc290b991eb5
dunedain289 <petersen@dunedain289.com>
2017-03-30 00:25:41 -0500

    trigger harvester role

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var harvesterRole = require('role.harvester');
+
 module.exports.loop = function() {
-  console.log("how quick on the updates?");
+  for (var cn in Game.creeps) {
+    var creep = Game.creeps[cn];
+    if (creep.memory.role == 'harvester') {
+      harvesterRole.run(creep);
+    }
+  }
 }
